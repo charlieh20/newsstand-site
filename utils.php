3b0a651402e83a6d8946d15ef22beb8aa2e82683
charlieh20 <60665390+charlieh20@users.noreply.github.com>
2022-06-13 17:39:08 -0700

    Update utils.php

--- utils.php
+++ utils.php
@@ -1,9 +1,7 @@
 <?php
 function setupDB()
 {
-	$db = new PDO(
-		  "mysql:dbname=1920project;host=mysql.1920.lakeside-cs.org",
-		  "student1920", "m545CS41920");
+	$db = new PDO("database");
 
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
